903eaa93fd9fb317d3db96e72b4566beeb4fd9e9
AyseTurk <ayse69turk@gmail.com>
2019-10-28 16:16:54 -0500

    second commit

---
+++ src/test/java/vy_TrackProject/Verification.java
@@ -0,0 +1,21 @@
+package vy_TrackProject;
+
+import java.util.ArrayList;
+
+public class Verification {
+    public static boolean verify(String expected, String actual) {
+
+        if (expected.equals(actual)) {
+           return true;
+
+        }
+
+          return false;
+
+
+
+        }
+
+
+    }
+
--- src/test/java/vy_TrackProject/Vy_TrackLoginFunctionality.java
+++ src/test/java/vy_TrackProject/Vy_TrackLoginFunctionality.java
@@ -5,39 +5,70 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import utils.BrowserFactory;
+import utils.BrowserUtils;
 
-import java.io.FileDescriptor;
+import java.util.ArrayList;
+
 
 public class Vy_TrackLoginFunctionality {
     /*
+    Task case
     •Go to the login page of VyTrack
     •Enter valid credential (can be any role)
     •Click login button
     •Verify that the user login successfully
      */
     public static void main(String[] args) {
-        WebDriver driver = BrowserFactory.getDriver("chrome");
-        driver.get("https://qa2.vytrack.com/user/login");
-        WebElement username = driver.findElement(By.name("_username"));
-        System.out.println(username);
-        username.sendKeys("storemanager99");
-        WebElement password = driver.findElement(By.name("_password"));
-        System.out.println(password);
-        password.sendKeys("UserUser123");
 
-        WebElement loginButton = driver.findElement(By.id("_submit"));
-        loginButton.submit();
 
-       driver.close();
+          // String array to hold user-names
+        String [] userNames = {"user165","user166","user167",
+                "storemanager98" , "storemanager99", "storemanager199",
+                "salesmanager261", "salesmanager262" };
+
+        loginFunctionality(userNames);
+
 
     }
-    public static void verifyLogin(String expect, String actual){
-        if (expect.equals(actual)) {
+    public static void loginFunctionality(String[] users){
+        ArrayList<Boolean> list = new ArrayList<>();
+        for(int i = 0; i < users.length; i++){
 
-            System.out.println("PASS");
+            // create  a driver object to get chrome browser
+            WebDriver driver = BrowserFactory.getDriver("chrome");
+            driver.get("https://qa2.vytrack.com/user/login");  // takes to vy_tracking application page
+
+            // locating elements for username and password
+            WebElement username = driver.findElement(By.name("_username"));
+            username.sendKeys(users[i]);
+            WebElement password = driver.findElement(By.name("_password"));
+            password.sendKeys("UserUser123");
+
+            // expected result
+            String expectedTitle = "Dashboard";
+
+            // find the element for login button
+            WebElement loginButton = driver.findElement(By.id("_submit"));
+            loginButton.submit();
+            BrowserUtils.wait(2);
+
+            // actual result
+            String actualTitle = driver.getTitle();
+
+            // call the verify method from the verification class
+          list.add(Verification.verify(expectedTitle, actualTitle));
+           driver.close(); // close the current page
         }
-        else{
-            System.out.println("FAiLED");
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i).equals(true)) {
+                System.out.println(users[i] + " can able to login");
+            }else{
+                System.out.println(users[i] + " cannot able to login");
+            }
         }
+
+
     }
+
+
 }
